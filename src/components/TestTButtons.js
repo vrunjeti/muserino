e9efb7767f1667f2d30c0621f85fc0efc87610dd
Varun Munjeti <vrunjeti@gmail.com>
2016-04-14 16:39:02 -0500

    drum sounds

--- src/components/TestTButtons.js
+++ src/components/TestTButtons.js
@@ -16,11 +16,12 @@ export default class TestTButtons extends React.Component {
   }
 
   componentDidMount() {
-    // React components load faster than scripts in the window, 
-    // so this waits to set T, the reference to Timbre 
+    // React components load faster than scripts in the window,
+    // so this waits to set T, the reference to Timbre
     setTimeout(() => {
+      console.log(window.T)
       T = window.T
-    }, 300)
+    }, 700)
   }
 
   /**
@@ -46,7 +47,7 @@ export default class TestTButtons extends React.Component {
     const freq = 110 * (index + 1)
 
     if (isPlaying[index]) {
-      // check if the Timbre object at the given index exists, otherwise create a new one 
+      // check if the Timbre object at the given index exists, otherwise create a new one
       if (tArr[index]) {
         tArr[index].play()
       } else {
@@ -61,24 +62,55 @@ export default class TestTButtons extends React.Component {
     this.setState({ tArr: tArr })
   }
 
+  playDrums() {
+    console.log(window.T)
+    T('audio').loadthis('../sounds/drum.wav', function() {
+      console.log('loaded')
+      var BD  = this.slice(   0,  500).set({bang:false});
+      var ksfsbkfl  = this.slice(   0,  500).set({bang:true});
+      var SD  = this.slice( 500, 1000).set({bang:false});
+      var HH1 = this.slice(1000, 1500).set({bang:false, mul:0.2});
+      var HH2 = this.slice(1500, 2000).set({bang:false, mul:0.2});
+      var CYM = this.slice(2000).set({bang:false, mul:0.2});
+      // var scale = new sc.Scale([0,1,3,7,8], 12, "Pelog");
+
+      // var P1 = [
+      //   [BD, HH1],
+      //   [HH1],
+      //   [HH2],
+      //   [],
+      //   [BD, SD, HH1],
+      //   [HH1],
+      //   [HH2],
+      //   [SD]
+      // ]
+      // .wrapExtend(128);
+
+      // var P2 = sc.series(16);
+
+      var drum = T("lowshelf", {freq:110, gain:8, mul:0.6}, BD, SD, HH1, HH2, CYM).play();
+    })
+  }
+
   render() {
     const { isPlaying } = this.state
     const buttonText = (index) => ((isPlaying[index]) ? 'Pause ' : 'Play ') + (index + 1)
-    
+
     return (
       <div>
         <div className="row"></div>
         <div className="row">
           {
             range(0, 5).map(num => {
-              return <button 
-                key={num} 
-                className="btn" 
+              return <button
+                key={num}
+                className="btn"
                 onClick={this.handlePlay.bind(this, num)}>
                 { buttonText(num) }
               </button>
             })
           }
+          <button className="btn" onClick={this.playDrums.bind(this)}>Drums</button>
         </div>
       </div>
     )
