b0e5e5e6ec2544668451ecf6e5aaf39d4030fdc9
Varun Munjeti <vrunjeti@gmail.com>
2016-04-15 10:21:26 -0500

    changed to 6 measures in grid view

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -9,10 +9,13 @@ export default class Timeline extends React.Component {
   }
 
   render() {
+    const NUM_MEASURES = 6
+    const TIME_INTERVAL = 2
+
     return (
       <div>
         <div className="row"></div>
-        <Times timeInterval={2} />
+        <Times timeInterval={TIME_INTERVAL} numMeasures={NUM_MEASURES} />
         {
           CHANNEL_DATA.map((channel, i) => {
             const { instrument, playedMeasures, color } = channel
@@ -20,8 +23,8 @@ export default class Timeline extends React.Component {
               <div className="row channel" key={'channel' + i}>
                 <div className="col s2">{ `Channel ${i+1} (${instrument})` }</div>
                 <div className="col s10 grid-container">
-                  <GridRow 
-                    numMeasures={4}
+                  <GridRow
+                    numMeasures={NUM_MEASURES}
                     index={i}
                     playedMeasures={playedMeasures}
                     color={color}
@@ -42,9 +45,10 @@ const GridRow = ({ numMeasures, index, playedMeasures, color }) => {
       {
         range(0, numMeasures).map((n, i) => {
           const backgroundColor = playedMeasures[i] ? color : '#fafafa'
-          return <div 
+          return <div
             className="grid-cell bordered-cell"
             style={{ backgroundColor: backgroundColor}}
+            numMeasures={numMeasures}
             key={i}>
           </div>
         })
@@ -53,13 +57,13 @@ const GridRow = ({ numMeasures, index, playedMeasures, color }) => {
   )
 }
 
-const Times = ({ timeInterval }) => {
+const Times = ({ timeInterval, numMeasures }) => {
   return (
     <div className="row times">
       <div className="col s2">Time</div>
       <div className="col s10">
       {
-        range(0, 4).map(n => n * timeInterval).map((n, i) => {
+        range(0, numMeasures).map(n => n * timeInterval).map((n, i) => {
           {/* TODO: format time to actual time, take in props of tempo to calculate */}
           return <div className="grid-cell" key={'time' + i}>Time: {n}</div>
         })
@@ -74,8 +78,8 @@ const Times = ({ timeInterval }) => {
  * Will be populated and recalculated by the actual music in the future
  */
 const CHANNEL_DATA = [
-  { instrument: 'drums', playedMeasures: [true, true, true, true], color: '#C3DDE6' },
-  { instrument: 'lead', playedMeasures: [false, true, false, true], color: '#DEC3E6' },
-  { instrument: 'chords', playedMeasures: [true, true, true, true], color: '#CBE6C3' },
-  { instrument: 'bass', playedMeasures: [false, false, false, false], color: '#E6CCC3' }
+  { instrument: 'drums', playedMeasures: [true, true, true, true, false, false], color: '#C3DDE6' },
+  { instrument: 'lead', playedMeasures: [false, true, false, true, false, false], color: '#DEC3E6' },
+  { instrument: 'chords', playedMeasures: [true, true, true, true, false, false], color: '#CBE6C3' },
+  { instrument: 'bass', playedMeasures: [false, false, false, false, false, false], color: '#E6CCC3' }
 ]
