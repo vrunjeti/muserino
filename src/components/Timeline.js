955871037c261da7a94f7e330c3fc1326e5d00ca
Varun Munjeti <vrunjeti@gmail.com>
2016-04-15 06:15:19 -0500

    merged color into channel data

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -13,12 +13,17 @@ export default class Timeline extends React.Component {
         <Times timeInterval={2} />
         {
           channelData.map((channel, i) => {
-            const { instrument, playedMeasures } = channel
+            const { instrument, playedMeasures, color } = channel
             return (
               <div className="row channel" key={'channel' + i}>
                 <div className="col s2">{ `Channel ${i+1} (${instrument})` }</div>
                 <div className="col s10 grid-container">
-                  <GridRow numMeasures={4} index={i} playedMeasures={playedMeasures}/>
+                  <GridRow 
+                    numMeasures={4}
+                    index={i}
+                    playedMeasures={playedMeasures}
+                    color={color}
+                  />
                 </div>
               </div>
             )
@@ -29,12 +34,12 @@ export default class Timeline extends React.Component {
   }
 }
 
-const GridRow = ({ numMeasures, index, playedMeasures }) => {
+const GridRow = ({ numMeasures, index, playedMeasures, color }) => {
   return (
     <div>
       {
         range(0, numMeasures).map((n, i) => {
-          const backgroundColor = playedMeasures[i] ? channelColors[index] : '#fafafa'
+          const backgroundColor = playedMeasures[i] ? color : '#fafafa'
           return <div 
             className="grid-cell bordered-cell"
             style={{ backgroundColor: backgroundColor}}
@@ -53,6 +58,7 @@ const Times = ({ timeInterval }) => {
       <div className="col s10">
       {
         range(0, 4).map(n => n * timeInterval).map((n, i) => {
+          {/* TODO: format time to actual time, take in props of tempo to calculate */}
           return <div className="grid-cell" key={'time' + i}>Time: {n}</div>
         })
       }
@@ -62,17 +68,10 @@ const Times = ({ timeInterval }) => {
 }
 
 const channelData = [
-  {instrument: 'drums', playedMeasures: [true, true, true, true]},
-  {instrument: 'lead', playedMeasures: [false, true, false, true]},
-  {instrument: 'chords', playedMeasures: [true, true, true, true]},
-  {instrument: 'bass', playedMeasures: [false, true, true, true]}
-]
-
-const channelColors = [
-  '#C3DDE6',
-  '#DEC3E6',
-  '#CBE6C3',
-  '#E6CCC3'
+  { instrument: 'drums', playedMeasures: [true, true, true, true], color: '#C3DDE6' },
+  { instrument: 'lead', playedMeasures: [false, true, false, true], color: '#DEC3E6' },
+  { instrument: 'chords', playedMeasures: [true, true, true, true], color: '#CBE6C3' },
+  { instrument: 'bass', playedMeasures: [false, true, true, true], color: '#E6CCC3' }
 ]
 
 /**
